ebabc0866cdeece4b91751e4d07d7bad0ed26220
kcode <kcode@kuaishou.com>
2021-01-17 10:51:15 +0800

    添加注释

--- src/main/java/org/apache/ibatis/logging/LogFactory.java
+++ src/main/java/org/apache/ibatis/logging/LogFactory.java
@@ -23,97 +23,97 @@ import java.lang.reflect.Constructor;
  */
 public final class LogFactory {
 
-  /**
-   * Marker to be used by logging implementations that support markers.
-   */
-  public static final String MARKER = "MYBATIS";
+    /**
+     * Marker to be used by logging implementations that support markers.
+     */
+    public static final String MARKER = "MYBATIS";
 
-  // 记录当前使用的第三方日志库适配器的构造方法
-  private static Constructor<? extends Log> logConstructor;
+    // 记录当前使用的第三方日志库适配器的构造方法
+    private static Constructor<? extends Log> logConstructor;
 
-  static {
-    // 尝试按照Slf4j、Common Loggin、Log4j2、Log4j、Jdk Logging、No Logging的顺序，
-    // 依次加载对应的适配器，一旦加载成功，就会记录到logConstructor字段中，并会停止后续适配器
-    tryImplementation(LogFactory::useSlf4jLogging);
-    tryImplementation(LogFactory::useCommonsLogging);
-    tryImplementation(LogFactory::useLog4J2Logging);
-    tryImplementation(LogFactory::useLog4JLogging);
-    tryImplementation(LogFactory::useJdkLogging);
-    tryImplementation(LogFactory::useNoLogging);
-  }
-
-  private LogFactory() {
-    // disable construction
-  }
-
-  public static Log getLog(Class<?> clazz) {
-    return getLog(clazz.getName());
-  }
-
-  public static Log getLog(String logger) {
-    try {
-      return logConstructor.newInstance(logger);
-    } catch (Throwable t) {
-      throw new LogException("Error creating logger for logger " + logger + ".  Cause: " + t, t);
+    static {
+        // 尝试按照Slf4j、Common Loggin、Log4j2、Log4j、Jdk Logging、No Logging的顺序，
+        // 依次加载对应的适配器，一旦加载成功，就会记录到logConstructor字段中，并会停止后续适配器
+        tryImplementation(LogFactory::useSlf4jLogging);
+        tryImplementation(LogFactory::useCommonsLogging);
+        tryImplementation(LogFactory::useLog4J2Logging);
+        tryImplementation(LogFactory::useLog4JLogging);
+        tryImplementation(LogFactory::useJdkLogging);
+        tryImplementation(LogFactory::useNoLogging);
     }
-  }
 
-  public static synchronized void useCustomLogging(Class<? extends Log> clazz) {
-    setImplementation(clazz);
-  }
-
-  public static synchronized void useSlf4jLogging() {
-    setImplementation(org.apache.ibatis.logging.slf4j.Slf4jImpl.class);
-  }
-
-  public static synchronized void useCommonsLogging() {
-    setImplementation(org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl.class);
-  }
-
-  public static synchronized void useLog4JLogging() {
-    setImplementation(org.apache.ibatis.logging.log4j.Log4jImpl.class);
-  }
-
-  public static synchronized void useLog4J2Logging() {
-    setImplementation(org.apache.ibatis.logging.log4j2.Log4j2Impl.class);
-  }
-
-  public static synchronized void useJdkLogging() {
-    setImplementation(org.apache.ibatis.logging.jdk14.Jdk14LoggingImpl.class);
-  }
-
-  public static synchronized void useStdOutLogging() {
-    setImplementation(org.apache.ibatis.logging.stdout.StdOutImpl.class);
-  }
-
-  public static synchronized void useNoLogging() {
-    setImplementation(org.apache.ibatis.logging.nologging.NoLoggingImpl.class);
-  }
-
-  private static void tryImplementation(Runnable runnable) {
-    if (logConstructor == null) {
-      try {
-        runnable.run();
-      } catch (Throwable t) {
-        // ignore
-      }
+    private LogFactory() {
+        // disable construction
     }
-  }
 
-  private static void setImplementation(Class<? extends Log> implClass) {
-    try {
-      // 获取implClass这个适配器的构造方法
-      Constructor<? extends Log> candidate = implClass.getConstructor(String.class);
-      // 尝试加载implClass这个适配器，加载失败会抛出异常
-      Log log = candidate.newInstance(LogFactory.class.getName());
-      if (log.isDebugEnabled()) {
-        log.debug("Logging initialized using '" + implClass + "' adapter.");
-      }
-      // 加载成功，则更新logConstructor字段，记录适配器的构造方法
-      logConstructor = candidate;
-    } catch (Throwable t) {
-      throw new LogException("Error setting Log implementation.  Cause: " + t, t);
+    public static Log getLog(Class<?> clazz) {
+        return getLog(clazz.getName());
     }
-  }
+
+    public static Log getLog(String logger) {
+        try {
+            return logConstructor.newInstance(logger);
+        } catch (Throwable t) {
+            throw new LogException("Error creating logger for logger " + logger + ".  Cause: " + t, t);
+        }
+    }
+
+    public static synchronized void useCustomLogging(Class<? extends Log> clazz) {
+        setImplementation(clazz);
+    }
+
+    public static synchronized void useSlf4jLogging() {
+        setImplementation(org.apache.ibatis.logging.slf4j.Slf4jImpl.class);
+    }
+
+    public static synchronized void useCommonsLogging() {
+        setImplementation(org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl.class);
+    }
+
+    public static synchronized void useLog4JLogging() {
+        setImplementation(org.apache.ibatis.logging.log4j.Log4jImpl.class);
+    }
+
+    public static synchronized void useLog4J2Logging() {
+        setImplementation(org.apache.ibatis.logging.log4j2.Log4j2Impl.class);
+    }
+
+    public static synchronized void useJdkLogging() {
+        setImplementation(org.apache.ibatis.logging.jdk14.Jdk14LoggingImpl.class);
+    }
+
+    public static synchronized void useStdOutLogging() {
+        setImplementation(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+    }
+
+    public static synchronized void useNoLogging() {
+        setImplementation(org.apache.ibatis.logging.nologging.NoLoggingImpl.class);
+    }
+
+    private static void tryImplementation(Runnable runnable) {
+        if (logConstructor == null) {
+            try {
+                runnable.run();
+            } catch (Throwable t) {
+                // ignore
+            }
+        }
+    }
+
+    private static void setImplementation(Class<? extends Log> implClass) {
+        try {
+            // 获取implClass这个适配器的构造方法
+            Constructor<? extends Log> candidate = implClass.getConstructor(String.class);
+            // 尝试加载implClass这个适配器，加载失败会抛出异常
+            Log log = candidate.newInstance(LogFactory.class.getName());
+            if (log.isDebugEnabled()) {
+                log.debug("Logging initialized using '" + implClass + "' adapter.");
+            }
+            // 加载成功，则更新logConstructor字段，记录适配器的构造方法
+            logConstructor = candidate;
+        } catch (Throwable t) {
+            throw new LogException("Error setting Log implementation.  Cause: " + t, t);
+        }
+    }
 
 }
